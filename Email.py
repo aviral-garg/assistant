f09e8964de4014bbeb75e603e972946dc76d04bf
Aviral Garg <contact@aviralgarg.com>
2020-03-30 15:05:43 -0700

    email

--- Email.py
+++ Email.py
@@ -1,12 +1,16 @@
 import smtplib
 
+from ENV.SECRET import USERNAME, PASSWORD
+
 
 class Email:
     def __init__(self):
         self.conn = smtplib.SMTP('smtp.gmail.com', 587)
         self.conn.ehlo()
         self.conn.starttls()
-        self.conn.login(USERNAME)
+        self.conn.login(USERNAME, PASSWORD)
+        self.conn.sendmail(USERNAME, USERNAME, 'Subject:So long...\n\nDear Aviral, here is a test email.\n\n-Aviral')
+        self.conn.quit()
 
 
 if __name__ == '__main__':
